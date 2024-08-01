cadf2b883279a88cc498061ca9becaae5efed5ef
MamtaPoudyal <157200718+MamtaPoudyal@users.noreply.github.com>
2024-08-01 13:02:25 +0530

    STUDYHUB
    Mamta Poudyal

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    // Handle form submission (e.g., send data to server)
+    // You can use AJAX or any other method here
+    // ...
+    console.log('Form submitted!');
+});
